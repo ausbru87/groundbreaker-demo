365bb1de1f3b994eaf74b3b89bec263432587642
ausbru87 <asbru17@gmail.com>
2023-06-27 17:34:50 -0600

    added ship image and null image generation - currently saving to file

--- EdgeDevice/ImageSensor.py
+++ EdgeDevice/ImageSensor.py
@@ -1,10 +1,37 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+from queue import LifoQueue
+import io
 import random
 import logging
-import queue 
+import queue
+import time
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('ImageSensor')
+
+def add_watermark(image):
+    # Create a transparent layer for the watermark
+    watermark = Image.new('RGBA', image.size, (0, 0, 0, 0))
+
+    # Set watermark text
+    text = "IMAGE IS UNCLASSIFIED - Generated Imagery"
+    font = ImageFont.truetype('/usr/share/fonts/dejavu/DejaVuSans.ttf', 20)
+
+
+    # Calculate text position
+    text_width, text_height = font.getsize(text)
+    x = image.width - text_width - 10
+    y = image.height - text_height - 10
+
+    # Draw the watermark text on the transparent layer
+    draw = ImageDraw.Draw(watermark)
+    draw.text((x, y), text, font=font, fill=(255, 255, 255, 128))
+
+    # Combine the original image and the watermark layer
+    watermarked_image = Image.alpha_composite(image.convert('RGBA'), watermark)
+
+    return watermarked_image
+
 
 class ImageSensor:
     def __init__(self, width, height):
@@ -12,24 +39,83 @@ class ImageSensor:
         self.height = height
         self.sensor_size = (self.width, self.height)
         self.capture_count = 0
-        self.image_queue = queue.LifoQueue(maxsize=500)
+        self.image_queue = LifoQueue(maxsize=500)
         logger.info(f'sensor initialized width {self.width} height {self.height}')
 
-    def capture_image(self):
-        if not self.image_queue.full():
-            self.capture_count += 1
-            image = Image.new("RGB", self.sensor_size)
-            image_pixels = image.load()
-            for i in range(image.size[0]):
-                for j in range(image.size[1]):
-                    image_pixels[i,j] = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-            logger.info(f'image captured {self.sensor_size}, current count: {self.capture_count}')
-            self.image_queue.put_nowait(image)
-        else:
-            logger.warn(f'image queue is currently full, cannot capture current request')
+    def generate_ship_image(self):
+        background_color = (58, 110, 165)  # Darker blue background
+        object_color = (105, 105, 105)  # Dark gray objects
+        text_color = (0, 0, 0)  # Black text color
+        size = self.sensor_size
+        # Create a new image with the specified size and background color
+        image = Image.new("RGB", size, background_color)
+        draw = ImageDraw.Draw(image)
+
+        # Randomly generate the size and position of the ship objects (ellipses in this case)
+        object_width = random.randint(80, 120)
+        object_height = random.randint(30, 70)
+        object_x = random.randint(0, size[0] - object_width)
+        object_y = random.randint(0, size[1] - object_height)
+        draw.ellipse((object_x, object_y, object_x + object_width, object_y + object_height), fill=object_color)
+
+        # Add the text "SHIP" in the middle of the ellipse
+        text = "SHIP"
+        text_width, text_height = draw.textsize(text)
+        text_x = object_x + (object_width - text_width) // 2
+        text_y = object_y + (object_height - text_height) // 2
+        draw.text((text_x, text_y), text, fill=text_color)
+
+        # Save image data to in-memory buffer
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, format='PNG')
+        image_data = image_buffer.getvalue()
+        
+        self.image_queue.put((image_data, 1))
+        return {'message': 'Geospatial image of ship generated and stored successfully'}
+
+    def generate_null_image(self):
+        background_color = (58, 110, 165)  # Darker blue background
+        size = self.sensor_size
+        # Create a new image with the specified size and background color
+        image = Image.new("RGB", size, background_color)
+        
+        # Save image data to in-memory buffer
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, format='PNG')
+        image_data = image_buffer.getvalue()
+        
+        self.image_queue.put((image_data, 0))
+        return {'message': 'Geospatial image of military base generated and stored successfully'}
+
+    def capture_images(self):
+        if self.image_queue.full():
+            logger.warn('Unable to capture image because the queue is full')
+            return {'message': 'Unable to capture image because the queue is full'}
+        while not self.image_queue.full():
+            selected_function = random.choice([self.generate_ship_image, self.generate_null_image])
+            selected_function()
 
     def set_sensor_size(self, width, height):
         self.width = width
         self.height = height
         self.sensor_size = (self.width, self.height)
-        logger.info(f'RESIZED SENSOR: WIDTH = {self.width} HEIGHT = {self.height}')
+        logger.info(f'resized sensor: {self.sensor_size}')
+    
+    def get_image(self):
+        if image_queue.empty():
+            logger.warn('No images available')
+            return jsonify({'message': 'No images available'})
+        image_data = image_queue.get()
+        logger.debug('image retrieved')
+        return  jsonify({'image': image_data.decode})
+    
+    def store_images(self):
+        while not self.image_queue.empty():
+            image_data = self.image_queue.get()
+            if image_data[1] == 1:
+                image = Image.open(io.BytesIO(image_data[0]))
+
+                watermarked_image = add_watermark(image)
+
+                watermarked_image.save(f"./images/generated-image_{self.capture_count}.png", "PNG")
+                self.capture_count += 1
