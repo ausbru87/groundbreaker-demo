66369b46fba17e72a9e45ec5b0be2f49f70dda26
ausbru87 <asbru17@gmail.com>
2023-06-27 12:40:00 -0600

    basic imagesensor for the edgedevice

--- EdgeDevice/ImageSensor.py
+++ EdgeDevice/ImageSensor.py
@@ -1,28 +1,35 @@
 from PIL import Image
 import random
+import logging
+import queue 
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('ImageSensor')
 
 class ImageSensor:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.sensor_size = (self.width, self.height)
-        self.image = None
-    
+        self.capture_count = 0
+        self.image_queue = queue.LifoQueue(maxsize=500)
+        logger.info(f'sensor initialized width {self.width} height {self.height}')
+
     def capture_image(self):
-        self.image = Image.new("RGB", self.sensor_size)
-        image_pixels = self.image.load()
-        for i in range(self.image.size[0]):
-            for j in range(self.image.size[1]):
-                image_pixels[i,j] = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-    
+        if not self.image_queue.full():
+            self.capture_count += 1
+            image = Image.new("RGB", self.sensor_size)
+            image_pixels = image.load()
+            for i in range(image.size[0]):
+                for j in range(image.size[1]):
+                    image_pixels[i,j] = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+            logger.info(f'image captured {self.sensor_size}, current count: {self.capture_count}')
+            self.image_queue.put_nowait(image)
+        else:
+            logger.warn(f'image queue is currently full, cannot capture current request')
+
     def set_sensor_size(self, width, height):
         self.width = width
         self.height = height
         self.sensor_size = (self.width, self.height)
-
-    def get_image(self):
-        return self.image
-    
-    def write_image_file(self):
-        self.image.save()
-        # TODO: add save info /images/raw and format
+        logger.info(f'RESIZED SENSOR: WIDTH = {self.width} HEIGHT = {self.height}')
---
+++ EdgeDevice/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from ImageSensor import ImageSensor
+
+images = []
+sensor = ImageSensor(width=640,height=640)
+app = Flask(__name__)
+
+@app.route('/sensor-data', methods=['POST'])
+def acquire_sensor_data():
+    # Acquire sensor data
+    # Replace this with actual code to capture sensor data
+    sensor_data = {'image': 'path/to/image'}
+    
+    # Send sensor data to Image Processing Microservice
+    # Replace with appropriate request to the Image Processing Microservice
+    # ...
+
+    return '', 200
+
+@app.route('/init-sensor', methods=['POST'])
+def initialize_sensor():
+    sensor = ImageSensor(640,640)
+    return '',200
+
+@app.route('/capture-images', methods=['POST'])
+def capture_images():
+    acc = 0
+    while acc < 502:
+        sensor.capture_image()
+        acc += 1
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
