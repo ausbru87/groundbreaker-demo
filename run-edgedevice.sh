3697f7a6a23a9398d915275bf59e9475a3419886
Austen Bruhn <abruhn@redhat.com>
2023-07-04 15:53:07 -0600

    updated volume setup

--- run-edgedevice.sh
+++ run-edgedevice.sh
@@ -1,5 +1,5 @@
 ## Pre-Run Cleanup
-IMAGES_VOL=groundbreaker-storage
+STORAGE_VOL=groundbreaker-storage
 SENSOR_NAME=sensor
 PROCESSOR_NAME=processor
 DOWNLINKER_NAME=downlinker
@@ -8,10 +8,10 @@ podman rm -f ${SENSOR_NAME}
 podman rm -f ${PROCESSOR_NAME}
 podman rm -f ${DOWNLINKER_NAME}
 
-podman volume rm ${IMAGES_VOL}
+podman volume rm ${STORAGE_VOL}
 
 ## Create Volume
-podman volume create ${IMAGES_VOL}
+podman volume create ${STORAGE_VOL}
 
 ## IMAGE SENSOR
 SENSOR_PORT=8081
@@ -20,7 +20,7 @@ SENSOR_IMAGE=quay.io/rhnspdev/groundbreaker-sensor:latest
 echo "Starting ${SENSOR_NAME}"
 podman run --name ${SENSOR_NAME} -d \
 	-p ${SENSOR_PORT}:8080 \
-	-v ${IMAGES_VOL}:/images \
+	-v ${STORAGE_VOL}:/images \
 	${SENSOR_IMAGE}
 
 ## IMAGE PROCESSOR
@@ -30,7 +30,7 @@ PROCESSOR_IMAGE=quay.io/rhnspdev/groundbreaker-processor:latest
 echo "Starting ${PROCESSOR_NAME}"
 podman run --name ${PROCESSOR_NAME} -d \
 	-p ${PROCESSOR_PORT}:8080 \
-	-v ${IMAGES_VOL}:/images \
+	-v ${STORAGE_VOL}:/images \
 	${PROCESSOR_IMAGE}
 
 ## IMAGE DOWNLINKER
@@ -40,7 +40,7 @@ DOWNLINKER_IMAGE=quay.io/rhnspdev/groundbreaker-downlinker:latest
 echo "Starting ${DOWNLINKER_NAME}"
 podman run --name ${DOWNLINKER_NAME} -d \
 	-p ${DOWNLINKER_PORT}:8080 \
-	-v ${IMAGES_VOL}:/images \
+	-v ${STORAGE_VOL}:/images \
 	-e "FACILITY_A=https://imagereceiver-groundbreaker-edgefac1.apps.groundbreaker-edgefac1.rhtps.io" \
 	-e "FACILITY_B=https://imagereceiver-groundbreaker-edgefac2.apps.groundbreaker-edgefac2.rhtps.io" \
 	${DOWNLINKER_IMAGE}
